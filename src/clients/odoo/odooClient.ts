6cb400edb6973d87e8a97a5293ed08e83306530b
Fabian Meyer <f@bian-meyer.de>
2018-11-07 14:56:05 +0100

    Implemented function to search for Odoo Client by RFID CARD UUID

--- src/clients/odoo/odooClient.ts
+++ src/clients/odoo/odooClient.ts
@@ -7,22 +7,21 @@ class OdooClient {
       port: 8069,
       db: 'odoo_fablab',
       username: 'f@bian-meyer.de',
-      password: 'IVm3O8nb0T13nOYlh9au'
+      password: 'IVm3O8nb0T13nOYlh9au',
     });
   }
 
   private odoo: any;
 
-  public async readRecord(): Promise<any> {
+  public async findUserByRfidUuid(rfidUuid : string): Promise<any> {
     try {
       await this.odoo.connect();
 
       const params = [
-                [['is_company', '=', true], ['customer', '=', true]], // ids to search for
-                ['name', 'country_id', 'comment'], // fields to return
-      ];
+        [['is_company', '=', false], ['customer', '=', true],
+         ['x_RFID_Card_UUID', '=', rfidUuid]], // ids to search for
+         ['x_RFID_Card_UUID', 'name']]; // fields to return
       const result = await this.odoo.execute_kw('res.partner', 'search_read', params);
-      const result2 = await this.odoo.exec_workflow('');
 
       console.log('result: ', result);
     } catch (e) {
@@ -32,6 +31,6 @@ class OdooClient {
 }
 
 const client = new OdooClient();
-client.readRecord();
+client.findUserByRfidUuid('9D:90:9C:1B');
 
 export default OdooClient;
