61320a9680eff7d08f8d40d6b9a662e08e82cbb1
Fabian Meyer <f@bian-meyer.de>
2019-03-04 11:36:57 +0100

    Refactored server.ts. Extracted route initalization to different file

---
+++ src/routes/index.ts
@@ -0,0 +1,68 @@
+import * as express from 'express';
+
+// Routes
+import userRoutes from './users/userRoutes';
+import devicesRoutes from './devices/devicesRoutes';
+import authRoutes from './auth/authRoutes';
+import bookingRoutes from './bookings/bookingRoutes';
+// Load passport and authentication configuration
+const passport = require('passport');
+require('../passport');
+
+// Load Config & necessary parameters
+const config = require('config');
+
+// Odoo Init
+import UserService from '../services/user/UserService';
+const odooXmlRpc = require('../libs/odoo-xmlrpc');
+import OdooClient from '../clients/odoo/OdooClient';
+const userService = new UserService(new OdooClient(new odooXmlRpc(config.get('odoo-client'))));
+
+// OpenHAB init
+import OpenhabClient from '../clients/openhab/OpenhabClient';
+import DeviceService from '../services/device/DeviceService';
+import { MongoClient, Db } from 'mongodb';
+import { DeviceBookingRepository } from '../repositories/DeviceBookingRepository';
+import { DeviceBookingService } from '../services/deviceBooking/DeviceBookingService';
+const deviceService = new DeviceService(new OpenhabClient(config.get('openhab-client')));
+
+const init = async (app: express.Application) => {
+  /**
+   * Database
+   */
+  let db: Db;
+  const { host, port, user, password, database } = config.get('mongodb');
+  const dbConnection = await MongoClient.connect(`mongodb://${user}:${password}@${host}:${port}/`, {
+    useNewUrlParser: true
+  });
+  db = dbConnection.db(database);
+
+  const deviceBookingRepository = new DeviceBookingRepository(db, 'deviceBookings');
+
+  // DeviceBooking Service init
+  const deviceBookingService = new DeviceBookingService(
+    deviceBookingRepository,
+    deviceService,
+    userService
+  );
+
+  app.get('*', function(req, res, next) {
+    console.log(`${req.originalUrl} was requested`);
+    return next();
+  });
+
+  app.use('/users', passport.authenticate('jwt', { session: false }), userRoutes(userService));
+  app.use(
+    '/devices',
+    passport.authenticate('jwt', { session: false }),
+    devicesRoutes(deviceService, userService, deviceBookingService)
+  );
+  app.use(
+    '/bookings',
+    passport.authenticate('jwt', { session: false }),
+    bookingRoutes(deviceService, userService, deviceBookingService)
+  );
+  app.use('/auth', authRoutes);
+};
+
+export default { init };
--- src/server.ts
+++ src/server.ts
@@ -12,84 +12,31 @@ const port: number = config.get('API.port');
 import errorHandler from './middleware/errorHandler';
 
 // Routes
-import UserService from './services/user/UserService';
-import userRoutes from './routes/users/userRoutes';
-import devicesRoutes from './routes/devices/devicesRoutes';
-import authRoutes from './routes/auth/authRoutes';
-import bookingRoutes from './routes/bookings/bookingRoutes';
-
-// Odoo Init
-const odooXmlRpc = require('./libs/odoo-xmlrpc');
-import OdooClient from './clients/odoo/OdooClient';
-const userService = new UserService(new OdooClient(new odooXmlRpc(config.get('odoo-client'))));
-
-// OpenHAB init
-import OpenhabClient from './clients/openhab/OpenhabClient';
-import DeviceService from './services/device/DeviceService';
-import { MongoClient, Db } from 'mongodb';
-import { DeviceBookingRepository } from './repositories/DeviceBookingRepository';
-import { DeviceBookingService } from './services/deviceBooking/DeviceBookingService';
-import { hostname } from 'os';
-const deviceService = new DeviceService(new OpenhabClient(config.get('openhab-client')));
-
-
-
-// Load passport and authentication configuration
-const passport = require('passport');
-require('./passport');
+import routes from './routes';
 
 (async () => {
-  /**
-   * Database
-   */
-  let db: Db;
   try {
-    const { host, port, user, password, database } = config.get('mongodb');
-    const dbConnection = await MongoClient.connect(`mongodb://${user}:${password}@${host}:${port}/`, {
-      useNewUrlParser: true
+    routes.init(app);
+    app.use(errorHandler);
+
+    app.listen(port, () => {
+      console.log(`Server started. Listening on port ${port}`);
     });
-    db = dbConnection.db(database);
+
+    io.on(
+      'connection',
+      socketioJwt.authorize({
+        secret: config.get('JWT').secret,
+        timeout: 15000 // 15 seconds to send the authentication message
+      })
+    ).on('authenticated', socket => {
+      console.log(`[Realtime API] Device connected ${socket.decoded_token.deviceID}`);
+    });
+
+    io.listen(8000);
+    console.log('socket.io listening on port 8000');
   } catch (e) {
-    console.error(e.stack);
+    console.error(e);
     process.exit(1);
   }
-  const deviceBookingRepository = new DeviceBookingRepository(db, 'deviceBookings');
-
-  // DeviceBooking Service init
-  const deviceBookingService = new DeviceBookingService(deviceBookingRepository, deviceService, userService); 
-
-  /**
-   * REST API
-   */
-  app.use('/users', passport.authenticate('jwt', { session: false }), userRoutes(userService));
-  app.use(
-    '/devices',
-    passport.authenticate('jwt', { session: false }),
-    devicesRoutes(deviceService, userService, deviceBookingService)
-  );
-  app.use('/bookings', passport.authenticate('jwt', { session: false }), bookingRoutes(deviceService, userService, deviceBookingService));
-  app.use('/auth', authRoutes);
-
-  // Error handler (Has to be on the end of the "pipeline")
-  app.use(errorHandler);
-
-  app.listen(port, () => {
-    console.log(`Server started. Listening on port ${port}`);
-  });
-
-  /**
-   * SOCKET - REALTIME STUFF
-   */
-  io.on(
-    'connection',
-    socketioJwt.authorize({
-      secret: config.get('JWT').secret,
-      timeout: 15000 // 15 seconds to send the authentication message
-    })
-  ).on('authenticated', socket => {
-    console.log(`[Realtime API] Device connected ${socket.decoded_token.deviceID}`);
-  });
-
-  io.listen(8000);
-  console.log('socket.io listening on port 8000');
 })();
