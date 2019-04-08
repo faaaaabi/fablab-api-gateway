313a28d9f7670909d721337e977d0d11f9d2d381
Fabian Meyer <f@bian-meyer.de>
2019-04-08 22:49:04 +0200

    Removed socket.io stuff from server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,5 @@
 import app from './app';
 
-// Socket IO
-const io = require('socket.io')();
-const socketioJwt = require('socketio-jwt');
-
 // Load Config & necessary parameters
 const config = require('config');
 const port: number = config.get('API.port');
@@ -23,33 +19,8 @@ import routes from './routes';
       console.log(`Server started. Listening on port ${port}`);
     });
 
-    io.on(
-      'connection',
-      socketioJwt.authorize({
-        secret: config.get('JWT').secret,
-        timeout: 15000 // 15 seconds to send the authentication message
-      })
-    ).on('authenticated', socket => {
-      console.log(`[Realtime API] Device connected ${socket.decoded_token.deviceID}`);
-      socket.on('advertise', payload => {
-        if (payload.accessDeviceName && payload.location) {
-          /*const realtimeAccessDevice = new RealtimeAccessDevice(
-            payload.accessDeviceName,
-            payload.location
-          );*/
-        }
-        socket.on('disconnect', () => {
-          console.log(`[Realtime API] Device disconnected ${socket.decoded_token.deviceID}`);
-        });
-      });
-    });
-
-    io.listen(8000);
-    console.log('socket.io listening on port 8000');
-
     process.on('SIGINT', function() {
       console.log('CLOSING [SIGINT]');
-      io.close();
       process.exit();
     });
   } catch (e) {
